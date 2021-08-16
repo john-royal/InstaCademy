080a58f6d8c2dc622aa6415bfac3c0a73a74eff4
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-08-15 23:28:09 -0500

    Update PostData.swift
    - Created a `DataType` enumeration
    -- A `DataType` will tell us what kind of posts we should be collecting for the posts property -- currently to include: all, singleAuthor, favorites.
    
    - Changed the `init` to include a `DataType`
    -- This requires that any call to PostData will ask for a specific type of `DataType`.
    -- Once a data type is selected, we then call `setPosts` to set the posts property to the desired posts
    
    - Created a `setPosts` function to set the posts property to the desired posts
    -- First we collect the favorites (This has to happen BEFORE the `self.posts = posts` statement in the other function, otherwise you end up with weird behavior, e.g. favorites not being annotated as favorites.)
    -- We then set our posts in one of the desired DataTypes. 
    -- For the desired posts, we annotate if they are a favorite
    
    - Created a `loadFavorites` function used to set posts as only those that are favorites
    -- This is used in the `.favorites` DataType
    
    - Created an `index(of post)` function to get the index of a Post
    -- Much like Cookcademy, I needed to get a Binding to a Post. This function allows me to get the index which I then use to get the binding.
    
    - Created a `remove(post)` function that returns if I successfully removed a post (although, it returns true if the post is found, not necessarily deleted)
    -- We then use the delete functions from the Post Model to delete the posts. 
    -- We remove it from the posts array

--- FirebaseTestProject/ViewModels/PostData.swift
+++ FirebaseTestProject/ViewModels/PostData.swift
@@ -9,10 +9,39 @@ import Foundation
 
 @MainActor class PostData: ObservableObject {
     @Published var posts: [Post] = []
+    var dataType: DataType
     
-    init() {
+    init(dataType: DataType) {
+        self.dataType = dataType
+        setPosts()
+    }
+    
+    func setPosts() {
         Task {
-            await loadPosts()
+            // Favorites HAS to go here for this to work correctly --
+            // the async calls (this one and the posts one) need to happen PRIOR to
+            // setting self.posts
+            let favorites = try await PostService.getFavorites()
+            
+            switch dataType {
+            case .all:
+                await loadPosts()
+            case .singleAuthor(let author):
+                await loadPosts()
+                posts = posts.filter({$0.author == author})
+            case .favorites:
+                await loadFavorites()
+            }
+            
+            // Placing it here removes the clunkiness of putting it inside
+            // each method, loadPosts and loadFavorites
+            // Each view needs to annotate favorites anyway, can place it here
+            let favoritesID = favorites.map({ $0.id })
+            for i in 0..<self.posts.count {
+                if favoritesID.contains(self.posts[i].id) {
+                    self.posts[i].isFavorite = true
+                }
+            }
         }
     }
     
@@ -24,5 +53,44 @@ import Foundation
         catch {
             print(error)
         }
+    }
+    
+    func loadFavorites() async {
+        do {
+            let posts = try await PostService.getFavorites()
+            self.posts = posts
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    func index(of post: Post) -> Int? {
+        for i in posts.indices {
+            if posts[i].id == post.id {
+                return i
+            }
+        }
+        return nil
+    }
+    
+    // Returns if Succeeded
+    func remove(post: Post) -> Bool {
+        // Returns if element not found
+        guard let index = posts.firstIndex(where: { $0.id == post.id }) else { return false }
+        posts.remove(at: index)
+        Task {
+            try await PostService.delete(post: post)
+        }
+        // We can return true here because we only want to know if the index exists
+        return true
+    }
+}
+
+extension PostData {
+    enum DataType {
+        case all
+        case singleAuthor(author: String)
+        case favorites
     }
 }
