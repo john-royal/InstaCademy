09ca70ce4e0e065dd72aa10051f640173190bb65
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-08-15 23:42:48 -0500

    Update PostsList.swift
    - Added a VStack inside the NavigationView that will hold:
    -- 1) SearchBar
    -- 2) ScrollView (holding the Posts)
    
    - Added a SearchBar (Custom View Added in Project) to Search the Posts
    
    - Inside the initial VStack, added a `posts` property that will filter out posts if the searchbar contains a current search
    
    - Added a ScrollView to display a `ForEach(posts)` instead of a List
    -- I did this because I was having incredible difficulty creating a space between the individual posts
    --- I wanted this space because I want a nice place to display comments, images, etc. 
    -- Used a `GeometryReader` to resize the ScrollView to a preferred size, e.g. 90% of the original width
    -- Used an alignment guide to align the ScrollView back to the middle of the original view, instead of the 10% space being on the trailing end
    
    - Created a `binding(for Post)` method, similar to Cookcademy, to grab a binding to an individual post. 
    
    - A PostRow now contains a deletePostAction to delete posts if the trash can is pressed on an individual post. 
    -- If it succeeds (it will return true if the index exists, not necessarily if the post is deleted), it will reset all the posts for that page.
    
    - I kept the comments of the List there for a reference, and didn't want to delete in-case we revert back to it.
    
    - Added a Toolbar to create a Post
    -- In essence, moving the Create Post to a ToolbarItem instead of a TabView
    -- The NewPostForm will now pop-up, to include a dismiss button now. 
    -- Additionally, on submit, the form is closed.
    -- Using the `.onDisappear` method, we are able to refresh our posts anytime the view disappears.

--- FirebaseTestProject/Views/PostsList.swift
+++ FirebaseTestProject/Views/PostsList.swift
@@ -8,25 +8,96 @@
 import SwiftUI
 
 struct PostsList: View {
-    @StateObject var postData = PostData()
+    @StateObject var postData: PostData
+    @State private var isPresenting: Bool = false
+    @State private var isEditing = false
+    @State private var searchText: String = ""
+    @Namespace var namespace
     
     var body: some View {
         NavigationView {
-            List(postData.posts, id: \.text) { post in
-                PostRow(post: post)
-            }
-            .navigationTitle("Posts")
-            .onAppear {
-                Task {
-                    await postData.loadPosts()
+            VStack {
+                // Searchable Posts
+                SearchBar(text: $searchText)
+                let posts = searchText == "" ?
+                                    postData.posts :
+                                    postData.posts.filter({ $0.postSearch(searchText) })
+                
+                let prefSize = 0.9
+                ScrollView(.vertical) {
+                    GeometryReader { g in
+                        VStack {
+                            ForEach(posts, id: \.id) { post in
+                                PostRow(post: binding(for: post), deletePostAction: { post in
+                                    let succeed = postData.remove(post: post)
+                                    if(succeed) {
+                                        postData.setPosts()
+                                    }
+                                })
+                            }
+                        }
+                        .frame(width: g.size.width*prefSize)
+                    }
                 }
+                .alignmentGuide(HorizontalAlignment.center, computeValue: { d in
+                    let scrollViewWidth = d.width*2
+                    let scrollViewWidthPrefSize = scrollViewWidth*prefSize
+                    let midwayPoint = (scrollViewWidth - scrollViewWidthPrefSize) / 2
+                    return d[HorizontalAlignment.center] - midwayPoint
+                })
+                // Unable to make List shorter width, centered, and space between items --
+                // switched to ForEach above -- 
+                // this is for adding comments and keeping a reasoanble spacing between posts
+//                List(posts, id: \.text) { post in
+//                    PostRow(post: binding(for: post), deletePostAction: { post in
+//                        let succeed = postData.remove(post: post)
+//                        if(succeed) {
+//                            postData.setPosts()
+//                        }
+//                    }).border(Color.black, width: 1)
+//                }
+                .navigationTitle("Posts")
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Create Post") {
+                            isPresenting = true
+                        }
+                    }
+                })
             }
+            .sheet(isPresented: $isPresenting, content: {
+                NavigationView {
+                    NewPostForm()
+                        .toolbar(content: {
+                            Button("Dismiss") {
+                                isPresenting = false
+                            }
+                        })
+                        // Need to reload postData (there is a more efficient way to do this, e.g. check for if not "Dismiss" pressed, but figured it was a minor case
+                        .onDisappear {
+                            Task {
+                                // Need to reset list of posts -- if user added a new post, it should appear
+                                // Additionally, this will change the list of posts for the Favorites page
+                                postData.setPosts()
+                            }
+                        }
+                }
+            })
+        }.background(Color.blue)
+    }
+}
+
+extension PostsList {
+    func binding(for post: Post) -> Binding<Post> {
+        guard let index = postData.index(of: post) else {
+            fatalError("Post not found")
         }
+        return $postData.posts[index]
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        PostsList()
+        PostsList(postData: PostData(dataType: .all))
     }
 }
