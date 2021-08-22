ed9955d3ebb8766b926d58dcf76d0f0ecad8b979
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-08-22 15:13:04 -0500

    Update Post.swift
    - Added a custom decoder
    -- Accomplishes 2 things:
    1. Can decode into specific properties so we don't get an error for trying to encode `isFavorite` because that should NOT be saved in the database (it would be near impossible in this manner). So, we use a custom decoder and initializer to allow for properties inside a Post that are not going to be stored into Firebase
    2. We can now decode/encode generically, for any data in Firebase. If Firebase has extra data, it will just not be inserted. If it does not have that variable, it will try and fail -- currently no catch and with my coding problem, not sure if it would error or move to a default string value, etc. 
    
    - Created a Favorite Structure to represent the Favorite collection in Firebase.
    -- This should contain a postid and userid. This is because a person cannot favorite more than 1 unique post. Therefore, the userid + postid will create a unique value. Leading into my next point...
    
    - Created a `getFavorites` function.  (NOT TESTED)
    -- This function gets all data from the favorites collection where the userid is equal to the current user. Thus, even if there are multiple posts with the same postid, it will only ever get unique posts because a user can only favorite a post 0 or 1 times. 
    
    - Created a delete function to delete from the Favorites collection in firebase.

--- FirebaseTestProject/Models/Post.swift
+++ FirebaseTestProject/Models/Post.swift
@@ -14,6 +14,7 @@ struct Post: FirebaseConvertable {
     let text: String
     let id: UUID
     let timestamp: Date
+    var isFavorite: Bool = false
     
     init(title: String, text: String, author: String) {
         self.title = title
@@ -22,7 +23,36 @@ struct Post: FirebaseConvertable {
         self.id = UUID()
         self.timestamp = Date()
     }
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case author
+        case text
+        case id
+        case timestamp
+    }
+    
+    //Custom Decoder for `isFavorite` since we don't set it using th Firebase.posts collections
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        title = try container.decode(String.self, forKey: .title)
+        author = try container.decode(String.self, forKey: .author)
+        text = try container.decode(String.self, forKey: .text)
+        id = try container.decode(UUID.self, forKey: .id)
+        timestamp = try container.decode(Date.self, forKey: .timestamp)
+    }
+    
     static let testPost = Post(title: "Title", text: "Content", author: "First Last")
+}
+
+struct Favorite: FirebaseConvertable {
+    let postid: UUID //Primary Key, so to speak
+    let userid: UUID
+    
+    init(postid: String, userid: String) {
+        self.postid = UUID(uuidString: postid)!
+        self.userid = UUID(uuidString: userid)!
+    }
 }
 
 protocol FirebaseConvertable: Codable {
@@ -49,13 +79,37 @@ struct PostService {
         return db.collection("posts")
     }
     
+    static var favoritesReference: CollectionReference {
+        let db = Firestore.firestore()
+        return db.collection("favorites")
+    }
+    
     static func getPosts() async throws -> [Post] {
         let postsSnapshots = try await postsReference.getDocuments()
         let posts = postsSnapshots.documents.map { Post(from: $0.data()) }
         return posts
     }
     
+    static func getFavorites() async throws -> [Post] {
+        let userid = UserDefaults.standard.value(forKey: "userid") != nil ?  UserDefaults.standard.value(forKey: "userid") as! String : "00854E9E-8468-421D-8AA2-605D8E6C61D9"
+        let favoritesQuery = favoritesReference.whereField("userid", isEqualTo: userid)
+        let favoritesSnapshots = try await favoritesQuery.getDocuments()
+        let favorites = favoritesSnapshots.documents.map { Post(from: $0.data()) }
+        
+        return favorites
+    }
+    
     static func upload(_ post: Post) async throws {
         try await postsReference.document(post.id.uuidString).setData(post.jsonDict)
+    }
+    
+    static func favorite(_ post: Post) async throws {
+        let userid = UserDefaults.standard.value(forKey: "userid") as? String ?? "00854E9E-8468-421D-8AA2-605D8E6C61D9"
+        try await favoritesReference.document(post.id.uuidString).setData(Favorite(postid: post.id.uuidString, userid: userid).jsonDict)
+    }
+    
+    // Added for the Delete functionality in the other branch
+    static func delete(favorite: Post) async throws {
+        try await favoritesReference.document(favorite.id.uuidString).delete()
     }
 }
