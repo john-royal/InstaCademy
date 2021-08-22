d01d069465163e340b96212c114bec46d3474481
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-08-22 01:33:25 -0500

    Update PostsList.swift
    - Added an `@EnvironmentObject postData` similar to how Cookcademy passed the RecipeData from the TabView
    
    - Added an `@State isPresenting` to change how the NewPostForm is displayed - instead of a TabView, it is displayed in the Toolbar.
    
    - Completely redid the entire PostRow and how it is displayed from the PostsList - to add spacing, easiness, etc.
    -- AlignmentGuide is used for both 1. Reducing the PostRow size to 90% of the ScrollView and 2. Centering the PostRow inside the ScrollView.
    
    - Added a `ViewStyle` to determine if we are presenting 1 of 3 options:
    1. All Posts
    2. Favorite Posts
    3. Posts by a Specific Author

--- FirebaseTestProject/Views/PostsList.swift
+++ FirebaseTestProject/Views/PostsList.swift
@@ -8,28 +8,93 @@
 import SwiftUI
 
 struct PostsList: View {
-    @StateObject var postData = PostData()
+    @EnvironmentObject private var postData: PostData
+    @State private var isPresenting: Bool = false
+    let viewStyle: ViewStyle 
     
     var body: some View {
         NavigationView {
-            List(postData.posts, id: \.text) { post in
-                PostRow(post: post)
-            }
-            .refreshable {
-                await postData.loadPosts()
-            }
-            .navigationTitle("Posts")
-            .onAppear {
-                Task {
-                    await postData.loadPosts()
+            VStack {
+                
+                let prefSize = 0.9
+                ScrollView(.vertical) {
+                    GeometryReader { g in
+                        VStack {
+                            ForEach(posts, id: \.id) { post in
+                                PostRow(post: binding(for: post), deletePostAction: { post in
+                                    postData.remove(post: post)
+                                }, isFavoriteAction: { post in
+                                    if post.isFavorite {
+                                        postData.favorites.append(post)
+                                    }
+                                    else {
+                                        guard let index = postData.favorites.firstIndex(where: { $0.id == post.id }) else { return }
+                                        postData.favorites.remove(at: index)
+                                    }
+                                })
+                            }
+                        }
+                        .frame(width: g.size.width*prefSize)
+                    }  
                 }
+                .alignmentGuide(HorizontalAlignment.center, computeValue: { d in
+                    let scrollViewWidth = d.width*2
+                    let scrollViewWidthPrefSize = scrollViewWidth*prefSize
+                    let midwayPoint = (scrollViewWidth - scrollViewWidthPrefSize) / 2
+                    return d[HorizontalAlignment.center] - midwayPoint
+                })
+                .navigationTitle("Posts")
+                .toolbar(content: {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Create Post") {
+                            isPresenting = true
+                        }
+                    }
+                })
             }
+            .sheet(isPresented: $isPresenting, content: {
+                NavigationView {
+                    NewPostForm()
+                        .toolbar(content: {
+                            Button("Dismiss") {
+                                isPresenting = false
+                            }
+                        })
+                }
+            })
+        }
+    }
+}
+
+extension PostsList {
+    enum ViewStyle {
+        case all
+        case favorites
+        case singleAuthor(String)
+    }
+    
+    func binding(for post: Post) -> Binding<Post> {
+        guard let index = postData.index(of: post) else {
+            fatalError("Post not found")
+        }
+        return $postData.posts[index]
+    }
+    
+    private var posts: [Post] {
+        switch viewStyle {
+        case .all:
+            return postData.posts
+        case .favorites:
+            return postData.favorites
+        case let .singleAuthor(author):
+            return postData.posts.filter({ $0.author == author })
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @StateObject static private var postData: PostData = PostData()
     static var previews: some View {
-        PostsList()
+        PostsList(viewStyle: .all)
     }
 }
