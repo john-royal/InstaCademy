54d79fdfab2fd1169fca7a722368f302d83472c7
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-08-22 15:17:46 -0500

    Update PostData.swift
    - Added a `favorites` array to hold current favorites
    
    - Created `loadFavorites` function to set the favorites in the same manner `loadPosts` works
    
    - Added a `remove` function to remove a Post (also in my other branches) - but including to remove a Post from favorite IFF it is a favorite.

--- FirebaseTestProject/ViewModels/PostData.swift
+++ FirebaseTestProject/ViewModels/PostData.swift
@@ -9,10 +9,20 @@ import Foundation
 
 @MainActor class PostData: ObservableObject {
     @Published var posts: [Post] = []
+    @Published var favorites: [Post] = []
     
     init() {
         Task {
             await loadPosts()
+            await loadFavorites()
+            
+            // Set Post.isFavorite for all favorited posts
+            let favoritesID = favorites.map({ $0.id })
+            for i in 0..<posts.count {
+                if favoritesID.contains(posts[i].id) {
+                    posts[i].isFavorite = true
+                }
+            }
         }
     }
     
@@ -24,5 +34,30 @@ import Foundation
         catch {
             print(error)
         }
+    }
+    
+    func loadFavorites() async {
+        do {
+            let favorites = try await PostService.getFavorites()
+            self.favorites = favorites
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    func remove(post: Post) {
+        // Will the task stop if we exit the function, prior to deletion?
+        Task {
+            try await PostService.delete(post: post)
+            try await PostService.
+        }
+        
+        guard let index = posts.firstIndex(where: { $0.id == post.id }) else { return }
+        posts.remove(at: index)
+        
+        // If is a favorite, we remove it as well. If not, it returns
+        guard let index = favorites.firstIndex(where: { $0.id == post.id }) else { return }
+        favorites.remove(at: index)
     }
 }
